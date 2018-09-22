f39459d66c38693553dd5b91014403510eab5eb1
OlegShvets <33119154+Buratinka@users.noreply.github.com>
2018-09-22 19:59:07 +0300

    Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,9 +2,9 @@ package main;
 
 public class Main {
     public static void main(String[] args) {
-        Cheating cheating = new Cheating();
-        cheating.setPath();
-        cheating.getIsMachine();
+        Parser parser = new Parser();
+        parser.setPath();
+        parser.getIsMachine();
 
     }
 }
